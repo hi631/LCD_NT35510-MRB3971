9413212fe56104291d854a2dee688724d6699fab
hi631 <hi631@ac.auone-net.jp>
2020-07-15 10:32:49 +0900

    NEW

---
+++ LCD/LCD.ino
@@ -0,0 +1,45 @@
+#include "MRB3971.h"
+
+#define LCD_CS   5
+#define LCD_RS   4
+#define LCD_RD  21
+#define LCD_WR  22
+#define LCD_RST 23     // Set.Hi (low level reset) 
+#define LCD_BL  xx     // Set.Hi (High level light)
+#define PARALLEL_0  12 // Byte Inpu Start 12(-19) Pin
+
+MRB3971 LCD = MRB3971(LCD_CS, LCD_RS, LCD_RD, LCD_WR, LCD_RST, PARALLEL_0);
+extern _lcd_dev lcddev;
+extern uint16_t POINT_COLOR, BACK_COLOR;
+
+//--  Demo  ----------------------------
+uint16_t rndw(){ return random(0,799);}
+uint16_t rndh(){ return random(0,479);}
+uint16_t rnd16(){ return random(0,65000);}
+
+void drawdemo(){
+  // Draw Demo
+  uint16_t sx=0,sy=0,ex,ey;
+  LCD.direction(3); LCD.SetWindow(0,0,lcddev.width-1,lcddev.height-1); LCD.FillWindow(BLACK);
+  for(int i=0;i<20;i++) { POINT_COLOR = rnd16(); LCD.Fill(rndw(),rndh(),rndw(),rndh());}
+  for(int i=0;i<10;i++) { POINT_COLOR = rnd16(); LCD.DrawCircle(rndw(),rndh(),rndh()/2+50);}
+  for(int i=0;i<10;i++) { POINT_COLOR = rnd16(); ex=rndw(); ey=rndh(); LCD.DrawLine(sx,sy,ex,ey); sx=ex; sy=ey;}
+  for(int i=0;i<10;i++) { POINT_COLOR = rnd16(); LCD.DrawRectangle(rndw(),rndh(),rndw(),rndh());}
+  for(int i=0;i<20;i++) LCD.paint(rndw(),rndh(),rnd16());
+  POINT_COLOR = WHITE; LCD.ShowString(10,10,(uint8_t *)"LCD Draw Demo");
+}
+//--  Start UP  --------------------------
+void setup() {            
+  Serial.begin(115200); Serial.println(""); Serial.print("Start\n");
+  LCD.Init(); 
+  Serial.printf("\r\nID= %X\r\n", LCD.Read_ID());
+  LCD.SetWindow(0,0,lcddev.width-1,lcddev.height-1);  LCD.FillWindow(0xffff);
+  drawdemo(); 
+}
+
+int ledmd = 0;
+void loop() {
+  delay(200); ledmd = 1-ledmd;
+  if(ledmd==0) LCD.ledon();  // digitalWrite(LED_BUILTIN, HIGH); 
+  else         LCD.ledoff(); // digitalWrite(LED_BUILTIN, LOW);
+}
---
+++ LCD/MRB3971.h
@@ -0,0 +1,83 @@
+// MRB3971.h
+//
+#ifndef MRB3971_LIBRARY
+#define MRB3971_LIBRARY
+#include <Arduino.h>
+
+typedef struct{                        
+  uint16_t width, height, id;
+  uint8_t  dir; 
+  uint16_t  wramcmd, rramcmd, setxcmd, setycmd;  
+} _lcd_dev; 
+
+#define LCD_W 480
+#define LCD_H 800
+#define USE_HORIZONTAL      0   // 0-0 1-90 2-180 3-270
+#define LCD_USE8BIT_MODEL   1   // 8Bit parallel Mode
+
+// color define
+#define WHITE      0xFFFF
+#define BLACK      0x0000   
+#define BLUE       0x001F  
+#define RED        0xF800
+#define GREEN      0x07E0
+#define CYAN       0x7FFF
+#define YELLOW     0xFFE0
+
+class MRB3971 {
+public:
+  MRB3971(uint8_t cs, uint8_t rs, uint8_t rd, uint8_t wr, uint8_t rst, uint8_t PARALLEL_0, uint8_t pin=2);
+  void parallel_setup(void);
+  void set_inputs(void);
+  void set_outputs(void);
+  uint8_t parallel_read(void);
+  void parallel_write(uint8_t value);
+  void write(uint8_t HVAL,uint8_t LVAL);
+  void writex2(uint8_t HVAL,uint8_t LVAL);
+  uint16_t read(void);
+  void WR_REG(uint16_t Reg);
+  void WR_DATA(uint16_t Data);
+  uint16_t RD_DATA(void);
+  void WriteReg(uint16_t Reg, uint16_t RegValue);
+  void ReadReg(uint16_t Reg,uint16_t *Rval,int n);
+  void WriteRAM_Prepare(void);
+  void FillWindow(uint16_t Color);
+  void Reset(void);
+  void Init(void);
+  void SetWindow(uint16_t xStar, uint16_t yStar,uint16_t xEnd,uint16_t yEnd);
+  void direction(uint8_t direction);
+  uint16_t Read_ID(void);
+
+  void DrawPoint(uint16_t x,uint16_t y);
+  uint16_t ReadPoint(uint16_t x,uint16_t y);
+  void ShowChar(uint16_t x,uint16_t y,uint8_t num,uint8_t mode);
+  void ShowString(uint16_t x,uint16_t y,uint8_t *p);
+  void DrawCircle(uint16_t x0,uint16_t y0,uint8_t r);
+  void Fill(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+  void DrawLine(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+  void DrawRectangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);  
+
+  void pset( uint16_t x, uint16_t y, uint16_t col );
+  uint16_t point( uint16_t x, uint16_t y );
+  void scanLine( uint16_t lx, uint16_t rx, uint16_t y, uint16_t col );
+  void paint( uint16_t x, uint16_t y, uint16_t paintCol );
+  
+  void ledon();     // LED ON
+  void ledoff();    // LED OFF
+  
+private:
+  uint8_t _cs;
+  uint8_t _rs;
+  uint8_t _rd;
+  uint8_t _wr;
+  uint8_t _rst;
+  uint32_t _csbit;
+  uint32_t _rsbit;
+  uint32_t _rdbit;
+  uint32_t _wrbit;
+  uint32_t _rstbit;
+  uint8_t _PARALLEL_0;
+  uint8_t _pin;
+};
+
+#endif
